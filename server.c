ff272e595069cea6846829cd06e451bd3a30289b
aditya m sharma <adityambha1999@gmail.com>
2023-11-06 22:18:24 +0530

    server.c

--- server.c
+++ server.c
@@ -1,111 +1,103 @@
 /*
-Flow of program
-START --> Initialize Winsock (socket library for windows) 
--->Create Server Socket --> Bind Server Socket --> Listen for incoming Connections
--->Accept Client Connection --> Serve Request to Client --> Send "Hello, World!" 
--->Make HTTP Request to External Server --> Receive and Print HTTP Response from External Server
--->Close Client Socket --> Repeat for New Connections 
---> END
+
+Start
+-->	Create a TCP socket
+-->	Bind the socket to the port 8080
+-->	Listen for incoming connections
+-->	Accept an incoming connection
+-->	Read the HTTP request from the client
+-->	Parse the HTTP request to determine the method and path
+-->	If the method is not GET
+	----->	Send a 405 Method Not Allowed response and close the connection
+-->	Else
+	----->	Get the parameters passed with the GET request
+-->	Generate the HTTP response
+-->	Send the HTTP response to the client
+-->	Close the connection
+End
 
 */
 
+
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <winsock2.h> //windows socket library
-#include <windows.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-#define ServerPort 8080
-#define TargetIp "127.0.0.1"
-#define TargetSPort 80
-
-void Serverreq(SOCKET client_soc) {
-    char response[] = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-    send(client_soc, response, sizeof(response), 0);
-}
-
-void http_req() {
-    WSADATA wsa;
-    SOCKET client_soc;
-    struct sockaddr_in ServerAddress;
-    int ServerAddress_len = sizeof(ServerAddress);
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        perror("WSAStartup failed");
-        return;
-    }
-    client_soc = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_soc == INVALID_SOCKET) {
-        perror("Socket creation failed");
-        WSACleanup();
-        return;
-    }
-    ServerAddress.sin_family = AF_INET;
-    ServerAddress.sin_port = htons(TargetSPort);
-    ServerAddress.sin_addr.s_addr = inet_addr(TargetIp);
-    if (connect(client_soc, (struct sockaddr*)&ServerAddress, ServerAddress_len) == SOCKET_ERROR) {
-        perror("Connection failed");
-        closesocket(client_soc);
-        WSACleanup();
-        return;
-    }
-
-    char req[] = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n";
-    send(client_soc, req, sizeof(req), 0);
-
-    char response[4096];
-    recv(client_soc, response, sizeof(response), 0);
-    printf("Received response:\n%s\n", response);
-    closesocket(client_soc);
-    WSACleanup();
-}
+#define PORT 8080
 
 int main() {
-    WSADATA wsa;
-    SOCKET server_soc, client_soc;
-    struct sockaddr_in ServerAddress, client_addr;
-    int client_addr_len = sizeof(client_addr);
+  // Create a TCP socket
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_fd < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        perror("WSAStartup failed");
-        return 1;
-    }
+  // Bind the socket to the port
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(PORT);
 
-    server_soc = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_soc == INVALID_SOCKET) {
-        perror("Socket creation failed");
-        WSACleanup();
-        return 1;
-    }
+  int bind_result = bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (bind_result < 0) {
+    perror("bind");
+    exit(EXIT_FAILURE);
+  }
 
-    ServerAddress.sin_family = AF_INET;
-    ServerAddress.sin_addr.s_addr = INADDR_ANY;
-    ServerAddress.sin_port = htons(ServerPort);
+  // Listen for incoming connections
+  int listen_result = listen(socket_fd, 10);
+  if (listen_result < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
 
-    if (bind(server_soc, (struct sockaddr*)&ServerAddress, sizeof(ServerAddress)) == SOCKET_ERROR) {
-        perror("Bind failed");
-        closesocket(server_soc);
-        WSACleanup();
-        return 1;
-    }
-    if (listen(server_soc, 5) == SOCKET_ERROR) {
-        perror("Listen failed");
-        closesocket(server_soc);
-        WSACleanup();
-        return 1;
-    }
-    printf("Server is listening on port %d...\n", ServerPort);
-    while (1) {
-        client_soc = accept(server_soc, (struct sockaddr*)&client_addr, &client_addr_len);
-        if (client_soc == INVALID_SOCKET) {
-            perror("Accept failed");
-            break;
-        }
-        Serverreq(client_soc);
-        http_req();
-        closesocket(client_soc);
-    }
-    closesocket(server_soc);
-    WSACleanup();
-    return 0;
+  // Accept an incoming connection
+  int client_socket_fd = accept(socket_fd, NULL, NULL);
+  if (client_socket_fd < 0) {
+    perror("accept");
+    exit(EXIT_FAILURE);
+  }
+
+  // Read the HTTP request
+  char request_buffer[1024];
+  int bytes_received = recv(client_socket_fd, request_buffer, sizeof(request_buffer), 0);
+  if (bytes_received < 0) {
+    perror("recv");
+    exit(EXIT_FAILURE);
+  }
+
+  // Parse the HTTP request
+  char *method = strtok(request_buffer, " ");
+  char *path = strtok(NULL, " ");
+
+  // Check if the request is a GET request
+  if (strcmp(method, "GET") != 0) {
+    // Not a GET request
+    send(client_socket_fd, "HTTP/1.1 405 Method Not Allowed\r\n\r\n", 32, 0);
+    close(client_socket_fd);
+    exit(EXIT_SUCCESS);
+  }
+
+  // Get the parameters passed with the GET request
+  char *parameters = strtok(NULL, "\r\n");
+
+  // Generate the HTTP response
+  char response_buffer[1024];
+  sprintf(response_buffer, "HTTP/1.1 200 OK\r\nContent-Encoding: utf-8\r\nContent-Type: text/html\r\n\r\n<h1>Hello, world!</h1>\n<p>Parameters: %s</p>\n", parameters);
+
+  // Send the HTTP response
+  int bytes_sent = send(client_socket_fd, response_buffer, sizeof(response_buffer), 0);
+  if (bytes_sent < 0) {
+    perror("send");
+    exit(EXIT_FAILURE);
+  }
+
+  // Close the sockets
+  close(client_socket_fd);
+  close(socket_fd);
+
+  return EXIT_SUCCESS;
 }
